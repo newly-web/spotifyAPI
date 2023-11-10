233627eaf31fe976d247bf73a769fcbfca4b6e96
smartblackgirl <whitneynsegbe@gmail.com>
2023-11-10 07:57:12 -0500

    scss-compiled

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database_name = "spotify api";
+
+$conn= new mysqli($servername,$username,$password,$database_name);
+
+?>
---
+++ search.php
@@ -0,0 +1,135 @@
+<?php
+require_once "connection.php"
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Black app</title>
+  <link rel="stylesheet" href="style.css">
+  <script src="https://kit.fontawesome.com/f6b7ebe716.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+  <?php
+  include_once "header.php";
+  ?>
+
+  <section class="userInteraction">
+    <h1>What are you looking for ?</h1>
+    <div class="search-container ">
+      <div class="searchInput">
+        <input type="search" name="userInput" id="" placeholder="Search for a topic, or even a creator">
+        <i class="fa-solid fa-magnifying-glass"></i>
+        <button>Filters</button>
+      </div>
+    </div>
+
+  </section>
+  <main>
+    <div class="results">
+      <div class="card">
+
+        <div class="image">
+          <img src="attachments/main_ng.png" alt="">
+        </div>
+        <h2 class="podcastName"> Podcast name </h2>
+
+        <div class="ratings">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+        </div>
+        <!-- <p class="podcastDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea ducimus pariatur modi magni mollitia corporis distinctio! At ex aliquid quibusdam perferendis! Magnam, cumque nostrum. Officia eius eaque itaque similique maiores.</p> -->
+      </div>
+      <div class="card">
+
+        <div class="image">
+          <img src="attachments/main_ng.png" alt="">
+        </div>
+        <h2 class="podcastName"> Podcast name </h2>
+
+        <div class="ratings">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+        </div>
+        <!-- <p class="podcastDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea ducimus pariatur modi magni mollitia corporis distinctio! At ex aliquid quibusdam perferendis! Magnam, cumque nostrum. Officia eius eaque itaque similique maiores.</p> -->
+      </div>
+      <div class="card">
+
+        <div class="image">
+          <img src="attachments/main_ng.png" alt="">
+        </div>
+        <h2 class="podcastName"> Podcast name </h2>
+
+        <div class="ratings">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+        </div>
+        <!-- <p class="podcastDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea ducimus pariatur modi magni mollitia corporis distinctio! At ex aliquid quibusdam perferendis! Magnam, cumque nostrum. Officia eius eaque itaque similique maiores.</p> -->
+      </div>
+      <div class="card">
+
+        <div class="image">
+          <img src="attachments/main_ng.png" alt="">
+        </div>
+        <h2 class="podcastName"> Podcast name </h2>
+
+        <div class="ratings">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+        </div>
+        <!-- <p class="podcastDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea ducimus pariatur modi magni mollitia corporis distinctio! At ex aliquid quibusdam perferendis! Magnam, cumque nostrum. Officia eius eaque itaque similique maiores.</p> -->
+      </div>
+      <div class="card">
+
+        <div class="image">
+          <img src="attachments/main_ng.png" alt="">
+        </div>
+        <h2 class="podcastName"> Podcast name </h2>
+
+        <div class="ratings">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+        </div>
+        <!-- <p class="podcastDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea ducimus pariatur modi magni mollitia corporis distinctio! At ex aliquid quibusdam perferendis! Magnam, cumque nostrum. Officia eius eaque itaque similique maiores.</p> -->
+      </div>
+      <div class="card">
+
+        <div class="image">
+          <img src="attachments/main_ng.png" alt="">
+        </div>
+        <h2 class="podcastName"> Podcast name </h2>
+
+        <div class="ratings">
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+        </div>
+        <!-- <p class="podcastDescription">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ea ducimus pariatur modi magni mollitia corporis distinctio! At ex aliquid quibusdam perferendis! Magnam, cumque nostrum. Officia eius eaque itaque similique maiores.</p> -->
+      </div>
+  </main>
+</body>
+
+</html>
